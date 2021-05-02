af9dcd6100b5b126dde389e5ecb987bbd62a64bb
lewinskim <lewinski.mar@gmail.com>
2021-05-02 12:56:59 +0200

    moved login logic from login component to loginservice

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { SecondauthService } from '../auth/secondauth.service';
+import { LoginService } from './login.service';
 import { AuthUser } from '../auth-user';
-import { AuthGuard } from '../auth.guard';
 
 @Component({
   selector: 'app-login',
@@ -14,34 +12,22 @@ export class LoginComponent implements OnInit {
   loggedUser: AuthUser = new AuthUser("", "");
 
   constructor(
-    private service: AuthService,
-    private secondAuthSrvc: SecondauthService,
-    private guard: AuthGuard
+    private loginService: LoginService,
   ) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    let isAuth: boolean = this.service.authenticate(this.loggedUser);
-    if (isAuth) {
-      this.login();
-    }
-  }
-
-  login(): void {
-    localStorage.setItem("token", "loggedin");
-    this.guard.setActivated(true);
+    this.loginService.login(this.loggedUser);
   }
 
   isLoggedIn(): boolean {
-    return localStorage.getItem("token") != null;
+    return this.loginService.isLoggedIn();
   }
 
   logout(): void {
-    localStorage.removeItem("token");
-    this.guard.setActivated(false);
-    this.secondAuthSrvc.deactivateUser();
+    this.loginService.logout();
   }
 
 }
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AuthUser } from '../auth-user';
+import { AuthService } from '../auth.service';
+import { AuthGuard } from '../auth.guard';
+import { SecondauthService } from '../auth/secondauth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private authService: AuthService,
+    private secondAuthService: SecondauthService,
+    private guard: AuthGuard
+  ) { }
+
+  login(user: AuthUser): void {
+    let isAuth: boolean = this.authService.authenticate(user);
+    if (isAuth) {
+      localStorage.setItem("token", "loggedin");
+      this.guard.setActivated(true);
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem("token");
+    this.secondAuthService.deactivateUser();
+    this.guard.setActivated(false);
+  }
+
+  public isLoggedIn(): boolean {
+    return localStorage.getItem("token") != null;
+  }
+}
